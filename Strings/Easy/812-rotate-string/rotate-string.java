dbfdb2d5b68fe9697479d31a1721fede1584557a
abhilashk12 <abhiabhilash74658@gmail.com>
2026-09-09 11:45:37 +0530

    Time: 1 ms (98.38%) | Memory: 43.6 MB (15.58%) - LeetSync

---
+++ Strings/Easy/812-rotate-string/rotate-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sb.toString().equals(goal)) {
+                return true;
+            }
+
+            char first = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(first);
+
+        }
+
+        return false;
+    }
+}
